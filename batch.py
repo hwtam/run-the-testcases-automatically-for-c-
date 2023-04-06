21b1a89a64b0d510b2f7189377019127a12a1916
hwtam <112061601+hwtam@users.noreply.github.com>
2023-04-06 14:08:18 +0800

    redo the main program

--- batch.py
+++ batch.py
@@ -1,12 +1,31 @@
 import os
+import glob
 
-a = "pa2 < testcase/input0"
-b = ".txt > myOutput0"
-c = ".txt"
+os.system("cls")
+file_name = glob.glob("*.exe")
+fn = file_name[0].split('.')[0]
+input_cases = glob.glob("./**/input*.txt")
+input_cases.extend(glob.glob("./input*.txt"))
 
-####### change this
-num = 8 
-####### change this
+print("Testing for : " + fn)
 
-for i in range(1, num + 1) :
-  os.system(a + str(i) + b + str(i) + c)
+try : 
+  for case in input_cases :
+    c = case.split('\\')
+    address = ''
+    cn = c[len(c)-1]
+    for i in range(1, len(c)-1) :
+      address += c[i] + '/'
+    address.rstrip('/')
+    
+    os.system(fn + " < " + address + cn + " > myOutput" + cn[5:])
+
+    with open("myOutput" + cn[5:]) as myOut, open(address + "output" + cn[5:]) as out :
+      if (myOut.read() == out.read()) :
+        print("No." + cn[5:-4] + " is okay.")
+      else :
+        print("No." + cn[5:-4] + " is WRONG!!!!")
+
+  print("DONE")
+except :
+  print("testing failed")
